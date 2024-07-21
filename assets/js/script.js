a4782097ed105afe91c46a295849028ff8eef0cd
sebastianalgharaballi <95605946+sebastianalgharaballi@users.noreply.github.com>
2024-07-21 12:54:02 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   const headerSteps = document.querySelectorAll('.header-step');
-
   function handleScroll() {
     const scrollPosition = window.scrollY;
     const windowHeight = window.innerHeight;
-
     headerSteps.forEach((step, index) => {
       const stepStart = index * windowHeight;
       const stepEnd = (index + 1) * windowHeight;
@@ -16,7 +14,22 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   }
-
   window.addEventListener('scroll', handleScroll);
   handleScroll(); // Initial call to set correct state
+
+  // New code for collapsible sections
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } 
+    });
+  }
 });
